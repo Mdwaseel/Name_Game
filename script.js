b77ec88ff47d1f5be4ad4fb6d39b903c2b032206
Mdwaseel <mdwaseel2311@gmail.com>
2023-12-05 21:04:44 +0530

    ss

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function redirect() {
     
     if (nameInput.toLowerCase() === "xyz") {
         window.location.href = "waseel.html";
-    } else if (nameInput.toLowerCase() === "sara") {
+    } else if (nameInput.toLowerCase() === "sarah") {
         window.location.href = "sara.html";
     } else {
         document.getElementById("errorMessage").innerHTML = "Please enter a correct name";
